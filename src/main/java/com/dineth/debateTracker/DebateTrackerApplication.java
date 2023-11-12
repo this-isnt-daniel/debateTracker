2e15e3fe2041396e89d509c5004aa000b831d54b
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-11-12 13:12:46 +0530

    initial commit

---
+++ src/main/java/com/dineth/debateTracker/DebateTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dineth.debateTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebateTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dineth/debateTracker/DebateTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineth.debateTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
