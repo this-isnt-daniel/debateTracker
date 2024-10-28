11a66945a32a0d3f532e9d90fb1643a92f069ff5
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2024-10-28 18:58:30 +0530

    Add finding judge X rounds including breaks

--- src/main/java/com/dineth/debateTracker/judge/JudgeRepository.java
+++ src/main/java/com/dineth/debateTracker/judge/JudgeRepository.java
@@ -10,8 +10,9 @@ import java.util.List;
 public interface JudgeRepository extends JpaRepository<Judge, Long> {
     Judge findByFnameAndLname(String fname, String lname);
 
+//    Get each judge and all the tournaments they've judged prelims at, along with the rounds they've judged
     @Query(value = "SELECT j.fname, j.lname, " +
-            "array_agg(DISTINCT r.round_name ORDER BY r.round_name) AS round_numbers, " +  // Added DISTINCT
+            "array_agg(DISTINCT r.round_name ORDER BY r.round_name) AS round_numbers, " +
             "t.short_name AS tournament_name " +
             "FROM ballot b " +
             "JOIN judge j ON b.judge_id = j.id " +
@@ -21,9 +22,22 @@ public interface JudgeRepository extends JpaRepository<Judge, Long> {
             "GROUP BY j.fname, j.lname, t.short_name " +
             "ORDER BY j.fname, j.lname",
             nativeQuery = true)
-    List<Object[]> getJudgesByTournamentWithRounds();
+    List<Object[]> getJudgesByTournamentWithPrelims();
+    //    Get each judge and all the tournaments they've judged breaks at, along with the rounds they've judged
+    @Query(value = "SELECT j.fname, j.lname, " +
+            "array_agg(DISTINCT r.round_name ORDER BY r.round_name) AS round_numbers, " +
+            "t.short_name AS tournament_name " +
+            "FROM elimination_ballot eb " +
+            "JOIN judge j ON eb.judge_id = j.id " +
+            "JOIN debate d ON eb.debate_id = d.id " +
+            "JOIN round r ON d.round_id = r.id " +
+            "JOIN tournament t ON r.tournament_id = t.id " +
+            "GROUP BY j.fname, j.lname, t.short_name " +
+            "ORDER BY j.fname, j.lname",
+            nativeQuery = true)
+    List<Object[]> getJudgesByTournamentWithBreaks();
 
-//    Get each judge and all the tournaments they've adjed at as an array
+//    Get each judge and all the tournaments they've judged prelims at
     @Query(value = "SELECT j.fname, j.lname, " +
             "array_agg(DISTINCT t.short_name ORDER BY t.short_name) AS tournament_names " +  // Added DISTINCT
             "FROM ballot b " +
@@ -34,6 +48,19 @@ public interface JudgeRepository extends JpaRepository<Judge, Long> {
             "GROUP BY j.fname, j.lname " +
             "ORDER BY j.fname, j.lname",
             nativeQuery = true)
-    List<Object[]> getJudgesByTournament();
+    List<Object[]> getJudgesByTournamentPrelims();
+
+//    Get each judge and all the tournaments they've judged breaks at
+    @Query(value = "SELECT j.fname, j.lname, " +
+            "array_agg(DISTINCT t.short_name ORDER BY t.short_name) AS tournament_names " +  // Added DISTINCT
+            "FROM elimination_ballot eb " +
+            "JOIN judge j ON eb.judge_id = j.id " +
+            "JOIN debate d ON eb.debate_id = d.id " +
+            "JOIN round r ON d.round_id = r.id " +
+            "JOIN tournament t ON r.tournament_id = t.id " +
+            "GROUP BY j.fname, j.lname " +
+            "ORDER BY j.fname, j.lname",
+            nativeQuery = true)
+    List<Object[]> getJudgesByTournamentBreaks();
 
 }
--- src/main/java/com/dineth/debateTracker/judge/JudgeService.java
+++ src/main/java/com/dineth/debateTracker/judge/JudgeService.java
@@ -4,9 +4,7 @@ import com.dineth.debateTracker.dtos.JudgeTournamentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class JudgeService {
@@ -33,33 +31,83 @@ public class JudgeService {
     }
 
     public List<JudgeTournamentDTO> getJudgesByTournamentWithRounds() {
-        List<Object[]> temp = judgeRepository.getJudgesByTournamentWithRounds();
-        List<JudgeTournamentDTO> result = new ArrayList<>();
-        for (Object[] obj : temp) {
+        List<Object[]> tempPrelims = judgeRepository.getJudgesByTournamentWithPrelims();
+        List<Object[]> tempBreaks = judgeRepository.getJudgesByTournamentWithBreaks();
+
+        HashMap<String, JudgeTournamentDTO> map = new HashMap<>();
+
+        // Process prelim rounds
+        for (Object[] obj : tempPrelims) {
             String firstName = (String) obj[0];
             String lastName = (String) obj[1];
-            String[] roundArray = (String[]) obj[2];  // Cast to String[]
-            List<String> rounds = Arrays.asList(roundArray);  // Convert to List<String>
+            String[] roundArray = (String[]) obj[2];
+            List<String> rounds = new ArrayList<>(Arrays.asList(roundArray));
             String tournamentShortName = (String) obj[3];
 
+            String key = firstName + " " + lastName + " " + tournamentShortName;
             JudgeTournamentDTO judgeTournamentDTO = new JudgeTournamentDTO(firstName, lastName, "", rounds, tournamentShortName);
-            result.add(judgeTournamentDTO);
+            map.put(key, judgeTournamentDTO);
         }
-        return result;
+
+        // Process break rounds
+        for (Object[] obj : tempBreaks) {
+            String firstName = (String) obj[0];
+            String lastName = (String) obj[1];
+            String[] roundArray = (String[]) obj[2];
+            List<String> rounds = Arrays.asList(roundArray);
+            String tournamentShortName = (String) obj[3];
+
+            String key = firstName + " " + lastName + " " + tournamentShortName;
+            JudgeTournamentDTO judgeTournamentDTO = map.get(key);
+
+            if (judgeTournamentDTO != null) {
+                judgeTournamentDTO.getRounds().addAll(rounds);  // Merge rounds if exists
+            } else {
+                judgeTournamentDTO = new JudgeTournamentDTO(firstName, lastName, "", rounds, tournamentShortName);
+                map.put(key, judgeTournamentDTO);  // Add to map if new
+            }
+        }
+
+        return new ArrayList<>(map.values());
     }
-    public List<JudgeTournamentDTO> getJudgesByTournament(){
-        List<Object[]> temp = judgeRepository.getJudgesByTournament();
-        List<JudgeTournamentDTO> result = new ArrayList<>();
-        for (Object[] obj : temp) {
+
+    public List<JudgeTournamentDTO> getJudgesByTournament() {
+        List<Object[]> tempPrelims = judgeRepository.getJudgesByTournamentPrelims();
+        List<Object[]> tempBreaks = judgeRepository.getJudgesByTournamentBreaks();
+
+        HashMap<String, JudgeTournamentDTO> map = new HashMap<>();
+
+        // Process prelim rounds
+        for (Object[] obj : tempPrelims) {
             String firstName = (String) obj[0];
             String lastName = (String) obj[1];
             String[] tournamentArray = (String[]) obj[2];  // Cast to String[]
             List<String> tournaments = Arrays.asList(tournamentArray);  // Convert to List<String>
 
             JudgeTournamentDTO judgeTournamentDTO = new JudgeTournamentDTO(firstName, lastName, "", tournaments, "");
-            result.add(judgeTournamentDTO);
+            String key = firstName + " " + lastName;
+            map.put(key, judgeTournamentDTO);
         }
-        return result;
+        // Process break rounds
+        for (Object[] obj : tempBreaks) {
+            String firstName = (String) obj[0];
+            String lastName = (String) obj[1];
+            String[] tournamentArray = (String[]) obj[2];  // Cast to String[]
+            List<String> tournaments = Arrays.asList(tournamentArray);  // Convert to List<String>
+
+            JudgeTournamentDTO judgeTournamentDTO = map.get(firstName + " " + lastName);
+            if (judgeTournamentDTO != null) {
+                //merge tournaments if exists and get distinct values
+                Set<String> set = new HashSet<>(judgeTournamentDTO.getRounds());
+                set.addAll(tournaments);
+                judgeTournamentDTO.setRounds(new ArrayList<>(set));
+                map.put(firstName + " " + lastName, judgeTournamentDTO);  // Add to map if exists
+            } else {
+                judgeTournamentDTO = new JudgeTournamentDTO(firstName, lastName, "", tournaments, "");
+                map.put(firstName + " " + lastName, judgeTournamentDTO);  // Add to map if new
+            }
+        }
+        return new ArrayList<>(map.values());
     }
 
 
